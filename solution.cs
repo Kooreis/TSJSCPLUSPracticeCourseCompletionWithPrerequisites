1a3f16d22f799d900e652590ab58db33d29b0805
Kooreis <kooreis.official@gmail.com>
2023-07-11 20:38:41 -0500

    This update introduces the CourseScheduler class with its constructor. The constructor initializes an adjacency list, a visited array, and a recursion stack array. It then populates the adjacency list with the prerequisites for each course.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,3 @@
-Here is a JavaScript solution using Depth-First Search (DFS) to detect cycles in a directed graph:
-
-```javascript
 class CourseScheduler {
     constructor(numCourses, prerequisites) {
         this.adjList = new Map();
@@ -15,48 +12,4 @@ class CourseScheduler {
             this.adjList.get(pre).push(course);
         }
     }
-
-    isCyclicUtil(i) {
-        if(this.recStack[i]) {
-            return true;
-        }
-
-        if(this.visited[i]) {
-            return false;
-        }
-
-        this.visited[i] = true;
-        this.recStack[i] = true;
-
-        let children = this.adjList.get(i);
-
-        for(let c = 0; c < children.length; c++) {
-            if(this.isCyclicUtil(children[c])) {
-                return true;
-            }
-        }
-
-        this.recStack[i] = false;
-        return false;
-    }
-
-    isCyclic() {
-        for(let node = 0; node < this.visited.length; node++) {
-            if(this.isCyclicUtil(node)) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
-
-function canFinish(numCourses, prerequisites) {
-    let courseScheduler = new CourseScheduler(numCourses, prerequisites);
-    return !courseScheduler.isCyclic();
-}
-
-console.log(canFinish(2, [[1,0]])); // true
-console.log(canFinish(2, [[1,0],[0,1]])); // false
-```
-
-This console application creates a CourseScheduler class that represents a directed graph of courses and their prerequisites. The `canFinish` function creates a new CourseScheduler and checks if there is a cycle in the graph. If there is a cycle, it means that there are circular dependencies between courses and not all courses can be finished, so it returns false. If there is no cycle, it means that all courses can be finished, so it returns true.
