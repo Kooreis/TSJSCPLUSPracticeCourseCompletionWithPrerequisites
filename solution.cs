4713c158fa444a30e8a68645605be379e2d32f44
Kooreis <kooreis.official@gmail.com>
2023-07-12 19:38:41 -0500

    This update adds the isCyclicUtil method to the CourseScheduler class. This method is a helper function for detecting cycles in the graph using Depth-First Search (DFS).

--- solution.cs
+++ solution.cs
@@ -1,15 +1,23 @@
-class CourseScheduler {
-    constructor(numCourses, prerequisites) {
-        this.adjList = new Map();
-        this.visited = new Array(numCourses).fill(false);
-        this.recStack = new Array(numCourses).fill(false);
-
-        for(let i = 0; i < numCourses; i++) {
-            this.adjList.set(i, []);
+isCyclicUtil(i) {
+        if(this.recStack[i]) {
+            return true;
         }
 
-        for(let [course, pre] of prerequisites) {
-            this.adjList.get(pre).push(course);
+        if(this.visited[i]) {
+            return false;
         }
+
+        this.visited[i] = true;
+        this.recStack[i] = true;
+
+        let children = this.adjList.get(i);
+
+        for(let c = 0; c < children.length; c++) {
+            if(this.isCyclicUtil(children[c])) {
+                return true;
+            }
+        }
+
+        this.recStack[i] = false;
+        return false;
     }
-}
